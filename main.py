a34a3bd5ce7137b604d7961671be08f8b02804b7
João Pedro Fernandes <joao.cobblepot7@gmail.com>
2024-12-19 01:12:19 -0300

    URL da Requisição da API da Marvel obtida com sucesso e conexão com servidor de dados estabelecida

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import hashlib
 import time
+import requests
 
+# Função para manipular os parâmetros e retornar a URL de Requisição
 def generate_parameter():
 
     # Chaves publicas e privadas (coloquei as minhas para obter meus parâmetros e testar o endpoint)
-    PUBLIC_KEY = "..."
-    PRIVATE_KEY = "..."
+    PUBLIC_KEY = "...coloque aqui sua chave pública..."
+    PRIVATE_KEY = "...coloque aqui sua chave privada..."
 
     # Valor do ts (timestamp) sendo a data e horário atual
     ts = str(time.time())
@@ -19,6 +21,22 @@ def generate_parameter():
     URL = f'http://gateway.marvel.com/v1/public/characters?limit=5&ts={ts}&apikey={PUBLIC_KEY}&hash={hash}'
 
     return URL
+
+
+# Obtendo a URL
+URL = generate_parameter()
+
+# Fzendo a requisição
+response = requests.get(URL)
+
+# Verificando se a requisição foi um sucesso ou não
+if response.status_code == 200:
+    data = response.json()  # Dados retornados pela API
+    print("A Requisição URL foi um sucesso!")
+else:
+    print(f"A Requisição URL falhou. Erro na requisição: {response.status_code}")
+    print(response.text)
+
 
 # Printando os valores
 #print("ts:", ts)
